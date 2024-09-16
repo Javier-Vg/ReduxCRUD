91a3910ea5e43f127494129aa435f1bb5a1cc8d3
Javier Vargas Guzman <Javier-Vg>
2024-09-15 22:17:37 -0600

    redux-sever-api

---
+++ Redux-Server/src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './components/home';
+import Userlisting from './components/Userlisting';
+import Updateuser from './components/Updateuser';
+import Adduser from './components/Adducer';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import Store from './Redux/Store';
+
+function App() {
+  return (
+    <Provider store={Store}>
+    <div className="App">
+      <BrowserRouter>
+        <div className='header'>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/user'}>User</Link>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/user' element={<Userlisting></Userlisting>}></Route>
+          <Route path='/user/add' element={<Adduser></Adduser>}></Route>
+          <Route path='/user/edit/:code' element={<Updateuser></Updateuser>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer className="toast-position"
+        position="bottom-right"></ToastContainer>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ Redux-Server/src/components/Userlisting.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { FetchUserList, Removeuser } from "../Redux/Action";
+
+const Userlisting = (props) => {
+
+  const [refresh, refreshSet] = useState(""); //Hace que renderize al eliminar
+
+  //renderiza la lista de usuarios apenas ingresar
+  useEffect(() => {
+    props.loaduser();
+  }, [refresh]);
+
+  const handledelete = (code) => {
+    if (window.confirm("¿Quieres eliminar este elemento?")) {
+      props.removeuser(code);
+      refreshSet(code) //Vuelve a montar la web
+      toast.success("User removed successfully.");
+    }
+  };
+
+  return props.user.loading ? (
+    <div>
+      <h2>Loading...</h2>
+    </div>
+  ) : props.user.errmessage ? (
+    <div>
+      <h2>{props.user.errmessage}</h2>
+    </div>
+  ) : (
+    <div>
+      <div className="card">
+        <div className="card-header">
+          <Link to={"/user/add"} className="btn btn-success">
+            Add User [+]
+          </Link>
+        </div>
+        <div className="card-body">
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>Code</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Role</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {props.user.userlist &&
+                props.user.userlist.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.role}</td>
+                    <td>
+                      <Link
+                        to={"/user/edit/" + item.id}
+                        className="btn btn-primary"
+                      >
+                        Edit
+                      </Link>{" "}
+                      |
+                      <button
+                        onClick={() => {
+                          handledelete(item.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loaduser: () => dispatch(FetchUserList()),
+    removeuser: (code) => dispatch(Removeuser(code)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Userlisting);
